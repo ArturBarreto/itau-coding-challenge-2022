9c3d9ddac9d29b00b05fd548d7996f503b85766e
ArturBarreto <artur.gomes.barreto@gmail.com>
2022-06-27 12:20:29 -0300

    OpenFeign habilitado

--- src/main/java/br/com/itau/codingchallenge2022/CodingChallenge2022Application.java
+++ src/main/java/br/com/itau/codingchallenge2022/CodingChallenge2022Application.java
@@ -2,7 +2,9 @@ package br.com.itau.codingchallenge2022;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CodingChallenge2022Application {
 
