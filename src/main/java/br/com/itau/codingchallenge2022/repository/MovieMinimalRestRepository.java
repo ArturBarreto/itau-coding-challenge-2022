62c56139685a2c3c9aa3d6f91dbe40788682710f
ArturBarreto <artur.gomes.barreto@gmail.com>
2022-06-28 13:41:20 -0300

    Reimplementação da classe movies, conclusão da comunicamção com o OMDB via OpenFeign e criação de teste unitário da comunicação com OMDB

--- src/main/java/br/com/itau/codingchallenge2022/repository/MovieMinimalRestRepository.java
+++ src/main/java/br/com/itau/codingchallenge2022/repository/MovieMinimalRestRepository.java
@@ -1,2 +1,12 @@
-package br.com.itau.codingchallenge2022.repository;public interface MovieMinimalRestRepository {
+package br.com.itau.codingchallenge2022.repository;
+
+import br.com.itau.codingchallenge2022.dto.ResultSearch;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "movieMinimalRest", url = "${omdbapi.url}")
+public interface MovieMinimalRestRepository {
+    @GetMapping
+    ResultSearch search(@RequestParam("s") String movieTitle);
 }
--- src/test/java/br/com/itau/codingchallenge2022/CodingChallenge2022ApplicationTests.java
+++ src/test/java/br/com/itau/codingchallenge2022/CodingChallenge2022ApplicationTests.java
@@ -1,13 +1,24 @@
 package br.com.itau.codingchallenge2022;
 
+import br.com.itau.codingchallenge2022.dto.ResultSearch;
+import br.com.itau.codingchallenge2022.repository.MovieMinimalRestRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CodingChallenge2022ApplicationTests {
 
+	@Autowired
+	private MovieMinimalRestRepository restRepository;
+
 	@Test
-	void contextLoads() {
+	void search() {
+		ResultSearch resultSearch = this.restRepository.search("lord of the rings");
+		assertTrue(resultSearch.getResponse());
+		resultSearch.getResultList().forEach(System.out::println);
 	}
 
 }
